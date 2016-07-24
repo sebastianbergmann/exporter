4fe2825f6bbda683635060e2c1d0b6140f761902
Junade Ali <junade@cloudflare.com>
2016-07-24 13:24:17 +0100

    Refactored Exporter.php to be PSR-1/PSR-2 compliant.

--- src/Exporter.php
+++ src/Exporter.php
@@ -38,8 +38,9 @@ class Exporter
      *  - Carriage returns and newlines are normalized to \n
      *  - Recursion and repeated rendering is treated properly
      *
-     * @param  mixed  $value
-     * @param  int    $indentation The indentation level of the 2nd+ line
+     * @param  mixed $value
+     * @param  int $indentation The indentation level of the 2nd+ line
+     *
      * @return string
      */
     public function export($value, $indentation = 0)
@@ -48,8 +49,9 @@ class Exporter
     }
 
     /**
-     * @param  mixed   $data
+     * @param  mixed $data
      * @param  Context $context
+     *
      * @return string
      */
     public function shortenedRecursiveExport(&$data, Context $context = null)
@@ -57,7 +59,7 @@ class Exporter
         $result   = array();
         $exporter = new self();
 
-        if (!$context) {
+        if ( ! $context) {
             $context = new Context;
         }
 
@@ -67,17 +69,13 @@ class Exporter
             if (is_array($value)) {
                 if ($context->contains($data[$key]) !== false) {
                     $result[] = '*RECURSION*';
-                }
-
-                else {
+                } else {
                     $result[] = sprintf(
                         'array(%s)',
                         $this->shortenedRecursiveExport($data[$key], $context)
                     );
                 }
-            }
-
-            else {
+            } else {
                 $result[] = $exporter->shortenedExport($value);
             }
         }
@@ -94,7 +92,8 @@ class Exporter
      * Newlines are replaced by the visible string '\n'.
      * Contents of arrays and objects (if any) are replaced by '...'.
      *
-     * @param  mixed  $value
+     * @param  mixed $value
+     *
      * @return string
      * @see    SebastianBergmann\Exporter\Exporter::export
      */
@@ -139,17 +138,18 @@ class Exporter
      * and public properties.
      *
      * @param  mixed $value
+     *
      * @return array
      */
     public function toArray($value)
     {
-        if (!is_object($value)) {
-            return (array) $value;
+        if ( ! is_object($value)) {
+            return (array)$value;
         }
 
         $array = array();
 
-        foreach ((array) $value as $key => $val) {
+        foreach ((array)$value as $key => $val) {
             // properties are transformed to keys in the following way:
             // private   $property => "\0Classname\0property"
             // protected $property => "\0*\0property"
@@ -196,9 +196,10 @@ class Exporter
     /**
      * Recursive implementation of export
      *
-     * @param  mixed                                       $value       The value to export
-     * @param  int                                         $indentation The indentation level of the 2nd+ line
-     * @param  \SebastianBergmann\RecursionContext\Context $processed   Previously processed objects
+     * @param  mixed $value The value to export
+     * @param  int $indentation The indentation level of the 2nd+ line
+     * @param  \SebastianBergmann\RecursionContext\Context $processed Previously processed objects
+     *
      * @return string
      * @see    SebastianBergmann\Exporter\Exporter::export
      */
@@ -235,13 +236,13 @@ class Exporter
             }
 
             return "'" .
-            str_replace(array("\r\n", "\n\r", "\r"), array("\n", "\n", "\n"), $value) .
-            "'";
+                   str_replace(array("\r\n", "\n\r", "\r"), array("\n", "\n", "\n"), $value) .
+                   "'";
         }
 
         $whitespace = str_repeat(' ', 4 * $indentation);
 
-        if (!$processed) {
+        if ( ! $processed) {
             $processed = new Context;
         }
 
