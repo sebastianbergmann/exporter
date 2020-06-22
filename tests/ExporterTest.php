9372d519fe5c280c918149b0d62f269d151a1729
Sebastian Bergmann <sb@sebastian-bergmann.de>
2020-06-22 16:36:01 +0200

    Fix CS/WS issues

--- tests/ExporterTest.php
+++ tests/ExporterTest.php
@@ -9,8 +9,20 @@
  */
 namespace SebastianBergmann\Exporter;
 
+use function array_map;
+use function chr;
+use function fopen;
+use function implode;
+use function mb_internal_encoding;
+use function mb_language;
+use function preg_replace;
+use function range;
+use Error;
+use Exception;
 use PHPUnit\Framework\TestCase;
 use SebastianBergmann\RecursionContext\Context;
+use SplObjectStorage;
+use stdClass;
 
 /**
  * @covers SebastianBergmann\Exporter\Exporter
@@ -29,12 +41,12 @@ class ExporterTest extends TestCase
 
     public function exportProvider(): array
     {
-        $obj2      = new \stdClass;
+        $obj2      = new stdClass;
         $obj2->foo = 'bar';
 
         $obj3 = (object) [1, 2, "Test\r\n", 4, 5, 6, 7, 8];
 
-        $obj = new \stdClass;
+        $obj = new stdClass;
         //@codingStandardsIgnoreStart
         $obj->null = null;
         //@codingStandardsIgnoreEnd
@@ -48,7 +60,7 @@ class ExporterTest extends TestCase
         $obj->array       = ['foo' => 'bar'];
         $obj->self        = $obj;
 
-        $storage = new \SplObjectStorage;
+        $storage = new SplObjectStorage;
         $storage->attach($obj2);
         $storage->foo = $obj2;
 
@@ -59,7 +71,7 @@ class ExporterTest extends TestCase
             'export int 1'                  => [1, '1'],
             'export float 1.0'              => [1.0, '1.0'],
             'export float 1.2'              => [1.2, '1.2'],
-            'export stream'                 => [\fopen('php://memory', 'r'), 'resource(%d) of type (stream)'],
+            'export stream'                 => [fopen('php://memory', 'r'), 'resource(%d) of type (stream)'],
             'export numeric string'         => ['1', "'1'"],
             'export multidimentional array' => [[[1, 2, 3], [3, 4, 5]],
                 <<<EOF
@@ -93,7 +105,7 @@ long\\n\\r
 text'
 EOF
             ],
-            'export empty stdclass'     => [new \stdClass, 'stdClass Object &%x ()'],
+            'export empty stdclass'     => [new stdClass, 'stdClass Object &%x ()'],
             'export non empty stdclass' => [$obj,
                 <<<EOF
 stdClass Object &%x (
@@ -154,15 +166,15 @@ stdClass Object &%x (
 EOF
             ],
             [
-                \chr(0) . \chr(1) . \chr(2) . \chr(3) . \chr(4) . \chr(5),
+                chr(0) . chr(1) . chr(2) . chr(3) . chr(4) . chr(5),
                 'Binary String: 0x000102030405',
             ],
             [
-                \implode('', \array_map('chr', \range(0x0e, 0x1f))),
+                implode('', array_map('chr', range(0x0e, 0x1f))),
                 'Binary String: 0x0e0f101112131415161718191a1b1c1d1e1f',
             ],
             [
-                \chr(0x00) . \chr(0x09),
+                chr(0x00) . chr(0x09),
                 'Binary String: 0x0009',
             ],
             [
@@ -170,7 +182,7 @@ EOF
                 "''",
             ],
             'export Exception without trace' => [
-                new \Exception('The exception message', 42),
+                new Exception('The exception message', 42),
                 <<<EOF
 Exception Object &%x (
     'message' => 'The exception message'
@@ -183,7 +195,7 @@ Exception Object &%x (
 EOF
             ],
             'export Error without trace' => [
-                new \Error('The exception message', 42),
+                new Error('The exception message', 42),
                 <<<EOF
 Error Object &%x (
     'message' => 'The exception message'
@@ -211,7 +223,7 @@ EOF
 
     public function testExport2(): void
     {
-        $obj      = new \stdClass;
+        $obj      = new stdClass;
         $obj->foo = 'bar';
 
         $array = [
@@ -291,7 +303,7 @@ EOF;
 
     public function shortenedExportProvider(): array
     {
-        $obj      = new \stdClass;
+        $obj      = new stdClass;
         $obj->foo = 'bar';
 
         $array = [
@@ -307,7 +319,7 @@ EOF;
             'shortened export numeric string' => ['1', "'1'"],
             // \n\r and \r is converted to \n
             'shortened export multilinestring'    => ["this\nis\na\nvery\nvery\nvery\nvery\nvery\nvery\rlong\n\rtext", "'this\\nis\\na\\nvery\\nvery\\nvery...\\rtext'"],
-            'shortened export empty stdClass'     => [new \stdClass, 'stdClass Object ()'],
+            'shortened export empty stdClass'     => [new stdClass, 'stdClass Object ()'],
             'shortened export not empty stdClass' => [$obj, 'stdClass Object (...)'],
             'shortened export empty array'        => [[], 'Array ()'],
             'shortened export not empty array'    => [$array, 'Array (...)'],
@@ -330,33 +342,33 @@ EOF;
      */
     public function testShortenedExportForMultibyteCharacters(): void
     {
-        $oldMbLanguage = \mb_language();
-        \mb_language('Japanese');
-        $oldMbInternalEncoding = \mb_internal_encoding();
-        \mb_internal_encoding('UTF-8');
+        $oldMbLanguage = mb_language();
+        mb_language('Japanese');
+        $oldMbInternalEncoding = mb_internal_encoding();
+        mb_internal_encoding('UTF-8');
 
         try {
             $this->assertSame(
                 "'いろはにほへとちりぬるをわかよたれそつねならむうゐのおくや...しゑひもせす'",
                 $this->trimNewline($this->exporter->shortenedExport('いろはにほへとちりぬるをわかよたれそつねならむうゐのおくやまけふこえてあさきゆめみしゑひもせす'))
             );
-        } catch (\Exception $e) {
-            \mb_internal_encoding($oldMbInternalEncoding);
-            \mb_language($oldMbLanguage);
+        } catch (Exception $e) {
+            mb_internal_encoding($oldMbInternalEncoding);
+            mb_language($oldMbLanguage);
 
             throw $e;
         }
 
-        \mb_internal_encoding($oldMbInternalEncoding);
-        \mb_language($oldMbLanguage);
+        mb_internal_encoding($oldMbInternalEncoding);
+        mb_language($oldMbLanguage);
     }
 
     public function provideNonBinaryMultibyteStrings(): array
     {
         return [
-            [\implode('', \array_map('chr', \range(0x09, 0x0d))), 9],
-            [\implode('', \array_map('chr', \range(0x20, 0x7f))), 96],
-            [\implode('', \array_map('chr', \range(0x80, 0xff))), 128],
+            [implode('', array_map('chr', range(0x09, 0x0d))), 9],
+            [implode('', array_map('chr', range(0x20, 0x7f))), 96],
+            [implode('', array_map('chr', range(0x80, 0xff))), 128],
         ];
     }
 
@@ -406,7 +418,7 @@ EOF;
             'export with numeric array key' => [[2 => 1], '1'],
             'export with assoc array key'   => [['foo' => 'bar'], '\'bar\''],
             'export multidimentional array' => [[[1, 2, 3], [3, 4, 5]], 'array(1, 2, 3), array(3, 4, 5)'],
-            'export object'                 => [[new \stdClass], 'stdClass Object ()'],
+            'export object'                 => [[new stdClass], 'stdClass Object ()'],
         ];
     }
 
@@ -423,6 +435,6 @@ EOF;
 
     private function trimNewline(string $string): string
     {
-        return \preg_replace('/[ ]*\n/', "\n", $string);
+        return preg_replace('/[ ]*\n/', "\n", $string);
     }
 }
