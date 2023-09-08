66aee9ca605cf2aa1c4532b6a852c293e3593e17
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-09-08 06:38:00 +0200

    Fix

--- src/Exporter.php
+++ src/Exporter.php
@@ -257,7 +257,7 @@ final class Exporter
         }
 
         if (is_string($value)) {
-            // Match for most non printable chars somewhat taking multibyte chars into account
+            // Match for most non-printable chars somewhat taking multibyte chars into account
             if (preg_match('/[^\x09-\x0d\x1b\x20-\xff]/', $value)) {
                 return 'Binary String: 0x' . bin2hex($value);
             }
