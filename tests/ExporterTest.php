e63685f2fc25ae7682d6bd9f63d7a0edac89ba1b
Sebastian Bergmann <sb@sebastian-bergmann.de>
2025-09-24 07:55:01 +0200

    Suppress warning

--- tests/ExporterTest.php
+++ tests/ExporterTest.php
@@ -40,7 +40,7 @@ class ExporterTest extends TestCase
 
         $storage = new \SplObjectStorage;
         $storage->offsetSet($obj2);
-        $storage->foo = $obj2;
+        @$storage->foo = $obj2;
 
         $resource = \fopen('php://memory', 'r');
         \fclose($resource);
